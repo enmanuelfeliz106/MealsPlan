b8d8700c847cbdd65ac34ad92acc205e91884f8e
Enmanuel Feliz <enmanuelfewliz106@gmail.com>
2020-04-23 20:24:32 -0500

    Conversion del label del item en boton
    Si pulsa puede decidir si pasarla al dia actual o no. Si elige que "si", se valida que la misma comida no este registrada para el dia actual.

--- src/app/historial-comidas/historial-comidas.page.ts
+++ src/app/historial-comidas/historial-comidas.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { PopoverController } from '@ionic/angular';
+import { PopoverController, AlertController } from '@ionic/angular';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
 import { PopoverComponent } from '../popover/popover.component';
 import * as firebase from 'firebase';
 import * as $ from 'jquery';
+import { Comida } from '../popover-agregar-comida/popover-agregar-comida.component';
 
 @Component({
   selector: 'app-historial-comidas',
@@ -13,18 +14,49 @@ import * as $ from 'jquery';
 })
 export class HistorialComidasPage {
 
+  validador: boolean; // valida que la comida no este en home, si lo esta, no la agrega
+  nuevaComida: Comida;
   idUsuario;
   comidas = [];
+  comidasDeHoy;
   idsDocument = [];
   favoritas = [];
+  hoy = new Date().toLocaleDateString();
   fecha: Date;
   select = 'fecha'; // campo
   palabraClave = '';
   
 
-  constructor(public popoverController: PopoverController, private autenticacion: AuthenticationService) {
+  constructor(public popoverController: PopoverController, private autenticacion: AuthenticationService,
+              public alert: AlertController) {
 
     this.idUsuario = firebase.auth().currentUser.uid;
+    this.obtenerComidasDeHoy();
+
+  }
+
+  obtenerComidasDeHoy() {
+    this.comidasDeHoy = [];
+    
+    let comida = firebase.firestore().collection('comidasGuardadas');
+    let query = comida.where('userID', '==', this.idUsuario).where('fecha', '==', this.hoy).get()
+        .then(snapshot => {
+          if (snapshot.empty) {
+            console.log('No matching documents.');
+            return;
+          }
+  
+          snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+            this.comidasDeHoy.push(doc.data());
+  
+          });
+        })
+        .catch(err => {
+          console.log('Error getting documents', err);
+        });
+
+    
 
   }
 
@@ -111,6 +143,122 @@ export class HistorialComidasPage {
     } 
      
 
+  }
+
+  pasarAHoy(index) {
+
+
+    this.nuevaComida = {
+        fecha: this.hoy,
+        userID: this.idUsuario,
+        comida: this.comidas[index].comida,
+        nombre: this.comidas[index].nombre,
+        ingredientes: this.comidas[index].ingredientes,
+        notas: this.comidas[index].notas,
+        calorias: this.comidas[index].calorias,
+        check: false,
+        favorita: false
+      };
+
+    let comidaReset: Comida;
+
+
+    for (let i = 0; i < this.comidasDeHoy.length; i++) {
+      comidaReset = {
+        fecha: this.hoy,
+        userID: this.comidasDeHoy[i].userID,
+        comida: this.comidasDeHoy[i].comida,
+        nombre: this.comidasDeHoy[i].nombre,
+        ingredientes: this.comidasDeHoy[i].ingredientes,
+        notas: this.comidasDeHoy[i].notas,
+        calorias: this.comidasDeHoy[i].calorias,
+        check: false,
+        favorita: false
+      };
+
+
+      if (this.nuevaComida.comida === comidaReset.comida &&
+          this.nuevaComida.nombre === comidaReset.nombre && 
+          this.nuevaComida.ingredientes === comidaReset.ingredientes &&
+          this.nuevaComida.fecha === comidaReset.fecha &&
+          this.nuevaComida.notas === comidaReset.notas && 
+          this.nuevaComida.calorias === comidaReset.calorias &&
+          this.nuevaComida.check === comidaReset.check &&
+          this.nuevaComida.favorita === comidaReset.favorita && 
+          this.nuevaComida.userID === comidaReset.userID) {
+
+        this.validador = true;
+        break;
+
+      } else {
+        this.validador = false;
+      }
+
+    }
+    
+    
+    if (this.validador === true) {
+      this.alertaError('Ya tienes esta comida registrada en las comidas para hoy.');
+
+    } else {
+      firebase.firestore().collection('comidasGuardadas').add(this.nuevaComida).then( (exito) => {
+      
+        this.alertaExito();
+
+        }).catch( (error) => {
+
+          this.alertaError('Consulta con tu proveedor.');
+
+        });
+    }
+    
+
+  }
+
+  async presentAlertPasarComida(index) {
+    const alert = await this.alert.create({
+      header: 'Pasar comida para hoy',
+      subHeader: '¿Seguro que desea hacerlo?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+        },
+        {
+          text: 'Si',
+          handler: () => {
+            this.pasarAHoy(index);
+          }
+        }]
+    });
+
+    await alert.present();
+  }
+
+  async alertaExito() {
+    const alert = await this.alert.create({
+      header: 'Exito',
+      subHeader: 'Comida guardada',
+      message: 'Puedes visualizarla en tu lista de comidas guardadas',
+      buttons: ['OK'],
+      cssClass: 'alertaExito'
+    });
+
+    await alert.present();
+    
+  }
+
+  async alertaError(mensaje) {
+    
+    const alert = await this.alert.create({
+          header: 'Error',
+          subHeader: 'Algo salio mal',
+          message: mensaje,
+          buttons: ['OK'],
+          cssClass: 'alertaError'
+        });
+    
+    await alert.present();
   }
 
 
